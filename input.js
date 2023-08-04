d951d45d1a61f44e1f9d38e0e68ed3d92c1c0acb
ralph <ralphdcouto1987@gmail.com>
2023-08-03 20:20:22 -0400

    updated all file to play the snak game

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
+let connection;
 
-
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,7 +15,21 @@ const setupInput = function () {
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else{
+    if (data === 'w') {
+      // console.log("Move Up");
+      connection.write("Move: up"); 
+    } else if (data === 'a') {
+      // console.log("Move Left");
+      connection.write("Move: left"); 
+    } else if (data === 's') {
+      // console.log("Move Down");
+      connection.write("Move: down"); 
+    } else if (data === 'd') {
+      // console.log("Move Right");
+      connection.write("Move: right"); 
+    }
   }
 };
 
-module.exports = setupInput
+module.exports = {setupInput}
